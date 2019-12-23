c3256602b55b0d6882c8affd68b0acb8c9cf1184
Mongker <49313037+Mongker@users.noreply.github.com>
2019-12-24 01:43:15 +0700

    BackUp di ngu

--- LO2-test1/xulyupdate.php
+++ LO2-test1/xulyupdate.php
@@ -1,3 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="refresh" content="0.2 url=<?php echo base_url() ?>../LO2-test1/test1.php" >
+    <title>Document</title>
+</head>
+<body>
 <?php
 $id=$_POST['id'];
 $hoten= $_POST['hoten'];
@@ -7,12 +18,17 @@ $gioitinh =$_POST['gioitinh'];
 $username = "root"; // Khai báo username
 $password = "";      // Khai báo password
 $server   = "localhost";   // Khai báo server
-$dbname   = "LO2-test1";      // Khai báo database     // Khai báo database
+$dbname   = "LO2-test1";      // Khai báo database
 $conn=mysqli_connect($server,$username,$password,$dbname) or die('không kết nối được database');
-$sql="UPDATE tin_xahoi SET hoten='$hoten', ngaysinh='$ngaysinh', gioitinh='$gioitinh'Where id='$id'";
+$sql="UPDATE test1 SET hoten='$hoten', ngaysinh='$ngaysinh', gioitinh='$gioitinh'Where id='$id'";
 $kq=mysqli_query($conn,$sql);
 if ($kq) {
-    header("location:../LO2-test1/Test1.php");
+//    header("./Test1.php");
 }
+?>
+</body>
+</html>
+
 
 ?>
+
