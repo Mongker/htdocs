327dfe380d67799af431dee1bc07899e745ad437
Mongker <49313037+Mongker@users.noreply.github.com>
2019-12-28 01:37:36 -0800

    Tạo phần mềm K3Office

---
+++ K3Office/insert.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Thêm mới sinh viên</title>
+</head>
+<body>
+    <?php
+        // $id=(isset($_GET['id']))?$_GET['id']:0;
+        $hoten=(isset($_POST['hoten']))? $_POST['hoten'] :"";
+        $ngaysinh=(isset($_POST['ngaysinh']))? $_POST['ngaysinh']:"";
+        $gioitinh=(isset($_POST['gioitinh']))? $_POST['gioitinh']:"";
+        $diachi=(isset($_POST['diachi']))? $_POST['diachi']:"";
+
+    //kết nối database
+        $servername= "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "K3Office";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }// echo "Connected successfully";
+
+        //insert dữ liệu
+        $sql_insert="insert into K3OfficeExcel values(null,'".$hoten."','".$ngaysinh."','".$gioitinh."','".$diachi."') ";
+            if ($hoten!='' && $ngaysinh!='' && $gioitinh!='' ) {
+               $conn->query($sql_insert);
+               echo' <script type="text/javascript">alert("Thêm mới thành công !");</script>';
+
+           }else {
+            echo '<script type="text/javascript">alert("Bạn phải nhập đầy đủ thông tin để thêm mới !");</script>';
+            }
+        $conn->close();
+
+     ?>
+     <form action="" method="post" enctype="multipart/form-data">
+         <table>
+             <tr>
+                 <td>Họ tên :</td>
+                 <td><input type="text" name="hoten"></td>
+             </tr>
+             <tr>
+                 <td>Ngày sinh :</td>
+                 <td><input type="date" name="ngaysinh"></td>
+             </tr>
+             <tr>
+                 <td>Giới tính</td>
+                 <td>
+                    <input type="radio" name="gioitinh" value="Nữ">Nữ
+                    <input type="radio" name="gioitinh" value="Nam">Nam
+                </td>
+             </tr>
+             <tr>
+                 <td>Địa chỉ:</td>
+                 <td><textarea name="diachi" style="width: 200px; height: 50px"></textarea></td>
+             </tr>
+             <tr>
+                 <td><input type="submit" name="them" value="Thêm mới"></td>
+                 <td><input type="reset" name="nhaplai" value="Reset"></td>
+                 <td><a href="test1.php">Xem danh sách</a></td>
+             </tr>
+         </table>
+     </form>
+
+
+</body>
+</html>
--- K3Office/xulyupdate.php
+++ K3Office/xulyupdate.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="0.5 url=http://localhost/LO2-test1/test1.php" >
+    <meta http-equiv="refresh" content="0.5 url=http://localhost/K3Office/test1.php" >
     <title></title>
 </head>
 <body>
@@ -16,11 +16,11 @@ $diachi =$_POST['diachi'];
 $username = "root"; // Khai báo username
 $password = "";      // Khai báo password
 $server   = "localhost";   // Khai báo server
-$dbname   = "LO2-test1";      // Khai báo database
+$dbname   = "K3Office";      // Khai báo database
 $conn=mysqli_connect($server,$username,$password,$dbname) or die('không kết nối được database');
-$sql="UPDATE test1 SET hoten='$hoten', ngaysinh='$ngaysinh', gioitinh='$gioitinh', diachi='$diachi' Where id='$id'";
+$sql="UPDATE K3OfficeExcel SET hoten='$hoten', ngaysinh='$ngaysinh', gioitinh='$gioitinh', diachi='$diachi' Where id='$id'";
 if ($kq) {
-    header("http://localhost/LO2-test1/test1.php");
+    header("http://localhost/K3Office/test1.php");
 }
 ?>
 </body>
